65ba134027a6c790cbcca5a12b534126847d6e76
michalwilk96 <michalwilk96@gmail.com>
2018-04-28 21:33:11 +0200

    Add method to split dataset to bucket by amount of lists

--- src/main/java/TicTacToeDataAnalyzer.java
+++ src/main/java/TicTacToeDataAnalyzer.java
@@ -18,4 +18,29 @@ public class TicTacToeDataAnalyzer {
         });
         return resultData;
     }
+
+    public void splitDataToBucket(List<List<Integer>> positiveDataSet,
+                                  List<List<Integer>> negativeDataSet,
+                                  List<List<Integer>> outputList) {
+        List<List<Integer>> foundNegativeData = getListsFromDataSetByAmount(33, negativeDataSet);
+        List<List<Integer>> foundPositiveData = getListsFromDataSetByAmount(62, positiveDataSet);
+        outputList.addAll(foundNegativeData);
+        outputList.addAll(foundPositiveData);
+    }
+
+    private List<List<Integer>> getListsFromDataSetByAmount(int numberOfLists, List<List<Integer>> inputList) {
+        List<List<Integer>> outputList = new ArrayList<>();
+
+        for (int i=0;i<numberOfLists;i++) {
+            List<Integer> foundList = inputList.get(i);
+            outputList.add(foundList);
+        }
+
+        for (int i=0;i<numberOfLists;i++) {
+            if (inputList.contains(outputList.get(i))) {
+                inputList.remove(outputList.get(i));
+            }
+        }
+        return outputList;
+    }
 }

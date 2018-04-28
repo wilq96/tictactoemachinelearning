841157f30c7867996a1f9a71361f2fa0e957d3f4
michalwilk96 <michalwilk96@gmail.com>
2018-04-28 22:15:57 +0200

    Add generating 10 dataset as csv from split positive, negative data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import csvutils.CsvProcessor;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         TicTacToeDataAnalyzer ticTacToeDataAnalyzer = new TicTacToeDataAnalyzer();
+        CsvProcessor csvProcessor = new CsvProcessor();
 
         List<List<Integer>> lostDataSet = ticTacToeDataAnalyzer.getPositiveOrNegativeDataByClass(0); //332
         List<List<Integer>> winDataSet = ticTacToeDataAnalyzer.getPositiveOrNegativeDataByClass(1); //626
@@ -29,5 +31,16 @@ public class Main {
         ticTacToeDataAnalyzer.splitDataToBucket(winDataSet, lostDataSet, dataSet8);
         ticTacToeDataAnalyzer.splitDataToBucket(winDataSet, lostDataSet, dataSet9);
         ticTacToeDataAnalyzer.splitDataToBucket(winDataSet, lostDataSet, dataSet10);
+
+        csvProcessor.generateCsvFileFromList(dataSet1, "dataset1.csv");
+        csvProcessor.generateCsvFileFromList(dataSet2, "dataset2.csv");
+        csvProcessor.generateCsvFileFromList(dataSet3, "dataset3.csv");
+        csvProcessor.generateCsvFileFromList(dataSet4, "dataset4.csv");
+        csvProcessor.generateCsvFileFromList(dataSet5, "dataset5.csv");
+        csvProcessor.generateCsvFileFromList(dataSet6, "dataset6.csv");
+        csvProcessor.generateCsvFileFromList(dataSet7, "dataset7.csv");
+        csvProcessor.generateCsvFileFromList(dataSet8, "dataset8.csv");
+        csvProcessor.generateCsvFileFromList(dataSet9, "dataset9.csv");
+        csvProcessor.generateCsvFileFromList(dataSet10, "dataset10.csv");
     }
 }
